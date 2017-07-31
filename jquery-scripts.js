d62a63ab7fbf5d578cabdfaa6d23cc1b497a33bb
Steven Xie <steven.xie@outlook.com>
2017-07-31 00:32:02 -0400

    Added certificate-creator.

--- jquery-scripts.js
+++ jquery-scripts.js
@@ -61,6 +61,8 @@ function savePreferences(){
    appStorage.set('excel-file-directory', $("#selected-file-directory p").text());
    appStorage.set('name-column-selected', $("#name-column-form select").find(":selected").text());
    appStorage.set('email-column-selected', $("#email-column-form select").find(":selected").text());
+   appStorage.set('award-column-selected', $("#award-column-form select").find(":selected").text());
+   appStorage.set('identifier-column-selected', $("#identifier-column-form select").find(":selected").text());
    appStorage.set('completion-column-selected', $("#completion-column-form select").find(":selected").text());
 }
 
@@ -101,6 +103,12 @@ function initializeColumnOptions(){
          $("#email-column-form option").filter(function() {
             return $(this).text() === appStorage.get('email-column-selected');
          }).prop('selected', true);
+         $("#identifier-column-form option").filter(function() {
+            return $(this).text() === appStorage.get('identifier-column-selected');
+         }).prop('selected', true);
+         $("#award-column-form option").filter(function() {
+            return $(this).text() === appStorage.get('award-column-selected');
+         }).prop('selected', true);
          $("#completion-column-form option").filter(function() {
             return $(this).text() === appStorage.get('completion-column-selected');
          }).prop('selected', true);
@@ -133,6 +141,186 @@ function loadExcelNames(){
          }
       });
       awesomplete.list = arrayOfNames;
+      globalNameList = arrayOfNames;
       awesomplete.evaluate();
    });
+}
+
+var awesomplete;
+var globalNameList;
+
+function certifyClients(){
+   const clientNames = getSelectedClients();
+   const workbook = new Excel.Workbook();
+   workbook.xlsx.readFile(appStorage.get('excel-file-directory')).then(function(){
+      var nameSheetNumber;
+      var nameColumnNumber;
+      var emailSheetNumber;
+      var emailColumnNumber;
+      var identifierSheetNumber;
+      var identifierColumnNumber;
+      var awardSheetNumber;
+      var awardColumnNumber;
+
+      workbook.eachSheet(function(worksheet, sheetId){
+         worksheet.getRow(1).eachCell(function(cell, colNumber){
+            if (cell.value == appStorage.get('name-column-selected')){
+               nameColumnNumber = colNumber;
+               nameSheetNumber = sheetId;
+            }
+            else if (cell.value == appStorage.get('email-column-selected')) {
+               emailColumnNumber = colNumber;
+               emailSheetNumber = sheetId;
+            }
+            else if (cell.value == appStorage.get('identifier-column-selected')) {
+               identifierColumnNumber = colNumber;
+               identifierSheetNumber = sheetId;
+            }
+            else if (cell.value == appStorage.get('award-column-selected')) {
+               awardColumnNumber = colNumber;
+               awardSheetNumber = sheetId;
+            }
+         });
+      });
+
+      const emailWorksheet = workbook.getWorksheet(emailSheetNumber);
+      const nameWorksheet = workbook.getWorksheet(nameSheetNumber);
+      const awardWorksheet = workbook.getWorksheet(awardSheetNumber);
+      const identifierWorksheet = workbook.getWorksheet(identifierSheetNumber);
+      var arrayOfCertificateDocuments = [];
+
+      clientNames.forEach(function(clientName, arrayIndex){
+         nameWorksheet.getColumn(nameColumnNumber).eachCell(function(cell, rowNumber){
+            if ((cell.value != null) && (cell.value != appStorage.get('name-column-selected'))){
+               if (cell.value == clientName){
+                  var certificateDocumentSpecifications = {};
+                  certificateDocumentSpecifications.name = cell.value;
+
+                  if (emailWorksheet.getRow(rowNumber).getCell(emailColumnNumber).value != null){
+                     var emailValue;
+                     if(typeof emailWorksheet.getRow(rowNumber).getCell(emailColumnNumber).value != 'string'){
+                        emailValue = emailWorksheet.getRow(rowNumber).getCell(emailColumnNumber).value.text;
+                     }
+                     else{
+                        emailValue = emailWorksheet.getRow(rowNumber).getCell(emailColumnNumber).value;
+                     }
+                     certificateDocumentSpecifications.email = emailValue
+                  }
+                  else{
+                     certificateDocumentSpecifications.email = "(email unavailable)";
+                  }
+
+
+                  if (awardWorksheet.getRow(rowNumber).getCell(awardColumnNumber).value != null){
+                     certificateDocumentSpecifications.award =   awardWorksheet.getRow(rowNumber).getCell(awardColumnNumber).value;
+                  }
+                  else{
+                     certificateDocumentSpecifications.award = "(award name unavailable)";
+                  }
+
+                  if (identifierWorksheet.getRow(rowNumber).getCell(identifierColumnNumber).value != null){
+                     certificateDocumentSpecifications.identifier = identifierWorksheet.getRow(rowNumber).getCell(identifierColumnNumber).value;
+                  }
+                  else{
+                     certificateDocumentSpecifications.identifier = "(certificate identifier unavailable)";
+                  }
+
+                  arrayOfCertificateDocuments.push(certificateDocumentSpecifications);
+               }
+            }
+         });
+      });
+
+      arrayOfCertificateDocuments.forEach(function(specifications, index){
+         makePDF(specifications.name, specifications.email, specifications.award, specifications.identifier);
+      });
+   });
+}
+
+const PDFDocument = require('pdfkit');
+const moment = require('moment');
+const fs = require('fs');
+
+function makePDF(name, email, award, identifier){
+   const date = new Date();
+   var monthString;
+   if ((date.getMonth() + 1) < 10){
+      monthString = "0" + (date.getMonth() + 1).toString();
+   }
+   else{
+      monthString = (date.getMonth() + 1).toString();
+   }
+
+   var dateString;
+   if ((date.getDate() + 1) < 10){
+      dateString = "0" + (date.getDate() + 1).toString();
+   }
+   else{
+      dateString = (date.getDate() + 1).toString();
+   }
+
+   const fullIdentifier = date.getFullYear().toString() + monthString + dateString + "-" + identifier;
+
+   const pdf = new PDFDocument({
+      autoFirstPage: false
+   });
+
+   pdf.pipe(fs.createWriteStream("generated-content/" + fullIdentifier + ".pdf"));
+   pdf.addPage({
+    layout: "landscape"
+   });
+
+   pdf.registerFont('garamond', 'other_assets/pdf-generator-fonts/garamond.ttf');
+   pdf.registerFont('century-gothic', 'other_assets/pdf-generator-fonts/century-gothic.ttf');
+
+   pdf.image('other_assets/certificate-template/template-blank.jpg', 0, 0, {scale: 0.3275});
+   pdf.font('garamond').fillColor("#414141").fontSize(36).text(name,13,238,{
+      align: 'center'
+   });
+   pdf.fillColor("#414141").fontSize(22).text(award,13,369,{
+      align: 'center'
+   });
+   pdf.fontSize(14).fillColor("#414141").text(moment().format('LL'), -265, 492,{
+      align: 'center'
+   });
+   pdf.font('century-gothic').fillColor("#8C8C8C").fontSize(9).text(email,0,589.35,{
+      align: 'right',
+      width: 315,
+      height: 50
+   });
+   pdf.fillColor("#8C8C8C").text(fullIdentifier,420,589.35,{
+      align: 'left',
+      width: 315,
+      height: 50
+   });
+   pdf.end();
+}
+
+function searchAndRemoveFromArray(array, value){
+   var indexOfValue;
+   array.forEach(function(item, index){
+      if (item == value){
+         indexOfValue = index;
+      }
+   });
+   array.splice(indexOfValue, 1);
+}
+
+function getSelectedClients(){
+   var selectedClientNames = [];
+   $(".name-selection .top #selected-client-names-list li").each(function(){
+      selectedClientNames.push($(this).html());
+   });
+   return selectedClientNames;
+}
+
+function arrayContainsValue(array, value){
+   var arrayDoesIndeedContainTheAforementionedValue = false;
+   array.forEach(function(item, index){
+      if (item == value){
+         arrayDoesIndeedContainTheAforementionedValue = true;
+         return;
+      }
+   });
+   return arrayDoesIndeedContainTheAforementionedValue;
 }
